a9dec58765fe2abcc910e67b58f5c2c201c875d6
LNgo00 <yusukeprezi@gmail.com>
2023-10-19 23:55:12 +0200

    add loadbalancer to manage ec2 instances access

--- server/main.tf
+++ server/main.tf
@@ -6,9 +6,34 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "yusuke_server" {
+data "aws_subnet" "az_a" {
+    availability_zone = "eu-west-1a"
+}
+
+data "aws_subnet" "az_b" {
+    availability_zone = "eu-west-1b"
+}
+
+resource "aws_instance" "yusuke_server_1" {
     ami           = "ami-0694d931cee176e7d"
     instance_type = "t3.micro"
+    subnet_id = data.aws_subnet.az_a.id
+    vpc_security_group_ids = [aws_security_group.yusuke_security_group.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hola terraform" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    tags = {
+        Name = "yusuke_server"
+    }
+}
+
+resource "aws_instance" "yusuke_server_2" {
+    ami           = "ami-0694d931cee176e7d"
+    instance_type = "t3.micro"
+    subnet_id = data.aws_subnet.az_b.id
     vpc_security_group_ids = [aws_security_group.yusuke_security_group.id]
     user_data = <<-EOF
                 #!/bin/bash
@@ -25,11 +50,76 @@ resource "aws_security_group" "yusuke_security_group" {
     name = "first_server_sg"
     vpc_id = data.aws_vpc.default.id
     ingress {
-        cidr_blocks = [ "0.0.0.0/0" ]
+        security_groups = [ aws_security_group.alb.id ]
         description = "Allow all traffic from the internet"
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
     }
 
+}
+
+resource "aws_lb" "alb" {
+    load_balancer_type = "application"
+    name               = "terraform-alb"
+    security_groups    = [aws_security_group.alb.id]
+    subnets = [data.aws_subnet.az_a.id, data.aws_subnet.az_b.id]
+}
+
+resource "aws_security_group" "alb" {
+    name = "alb-sg"
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow all traffic from the internet"
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+    }
+
+    egress {
+        to_port = 8080
+        from_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow access to our servers from port 8080"
+    }
+}
+
+resource "aws_lb_target_group" "this" {
+    name = "terraform-alb-target-group"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        enabled = true
+        matcher = "200"
+        path = "/"
+        port = "8080"
+        protocol = "HTTP"
+    }
+}
+
+resource "aws_lb_target_group_attachment" "yusuke_server_1" {
+    target_group_arn = aws_lb_target_group.this.arn # arn is Amazon Resource Name
+    target_id = aws_instance.yusuke_server_1.id
+    port = 8080
+}
+
+resource "aws_lb_target_group_attachment" "yusuke_server_2" {
+    target_group_arn = aws_lb_target_group.this.arn # arn is Amazon Resource Name
+    target_id = aws_instance.yusuke_server_2.id
+    port = 8080
+}
+
+resource "aws_lb_listener" "this" {
+    load_balancer_arn = aws_lb.alb.arn
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+      target_group_arn = aws_lb_target_group.this.arn
+      type             = "forward"
+    }
 }
--- server/output.tf
+++ server/output.tf
@@ -1,9 +1,14 @@
-output "public_dns" {
+output "public_dns_serv_1" {
     description = "public DNS of server"
-    value = "http://${aws_instance.yusuke_server.public_dns}:8080"
+    value = "http://${aws_instance.yusuke_server_1.public_dns}:8080"
 }
 
-output "public_ipv4" {
-    description = "public ipv4 of server"
-    value = aws_instance.yusuke_server.public_ip
+output "public_dns_serv_2" {
+    description = "public DNS of server"
+    value = "http://${aws_instance.yusuke_server_2.public_dns}:8080"
+}
+
+output "public_dns_loadbalancer" {
+    description = "public DNS of server"
+    value = "http://${aws_lb.alb.dns_name}"
 }
